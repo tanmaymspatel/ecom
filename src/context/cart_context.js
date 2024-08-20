d28ee08a276365f55c44ea811e1d1f9a06892b5f
Tanmay Patel <tanmay.patel@1rivet.com>
2024-08-20 10:27:19 +0530

    changed shipping price

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -17,7 +17,7 @@ const initialState = {
   cart: getLocalCartData(),
   total_item: "",
   total_price: "",
-  shipping_fee: 50000,
+  shipping_fee: 40000,
 };
 
 const CartProvider = ({ children }) => {
